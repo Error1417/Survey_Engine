2bdab9c864af50b77c6e624d8c562e2fe9c3d3ca
Error1417 <101829080+Error1417@users.noreply.github.com>
2023-04-23 19:04:16 -0700

    Add files via upload

---
+++ Survey_Engine_V3/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: error
+ *
+ * Created on April 18, 2023, 9:18 PM
+ */
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include "survey.h"
+
+using namespace std;
+
+int main() {
+
+    Survey survey = runSurveyCreator();
+    return 0;
+
+}
+
+
+
+
